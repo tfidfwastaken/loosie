0fc39da0a1314828875e706d3b74527a7e4b1c92
Atharva Raykar <raykar.ath@gmail.com>
2020-07-03 20:40:52 +0530

    Add basic upload functionality
    is very hardcoded and sux

--- app.rkt
+++ app.rkt
@@ -1,12 +1,15 @@
 #lang web-server/base
 
 (require racket/list
+         racket/file
 	 web-server/http
 	 web-server/stuffers
 	 web-server/page
 	 web-server/servlet
          web-server/servlet-env
-         web-server/dispatch)
+         web-server/formlets
+         web-server/dispatch
+         xml)
 
 (provide interface-version)
 (define interface-version 'stateless)
@@ -15,17 +18,44 @@
                  (string->number (getenv "PORT"))
                  8080))
 
-(define (counter cnt)
-  (page
-   (response/xexpr
-    `(html (head (title "loosie"))
-           (body (h1 ,(number->string cnt))
-                 (a ([href ,(embed/url
-                             (λ (req) (counter (add1 cnt))))])
-                    "Increment"))))))
+; render-uploader: request -> doesn't return
+(define (render-uploader request)
+  (define (upload-handler request)
+    (define-values (fname fcontents)
+      (formlet-process upload-formlet request))
+    (current-directory "/home/atharva/loosie/")
+    (display-to-file fcontents "test.html" #:exists 'replace)
+    (response/xexpr "SUCCESS"))
 
-(define (start req)
-  (counter 0))
+  ; renders the upload page
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html 
+       (head (title "loosie"))
+       (body (h1 "LOOSIE")
+             (form 
+              ([action ,(embed/url upload-handler)]
+               [method "POST"]
+               [enctype "multipart/form-data"])
+              ,@(formlet-display upload-formlet)
+              (input ([type "submit"] [value "Upload"])))))))
+
+(send/suspend/dispatch response-generator))
+
+; upload-formlet: formlet (binding?)
+(define upload-formlet
+  (formlet
+   (div ,{(file-upload) . => . binds})
+   ; (formlet-process upload-formlet request)
+   ; returns the file name and contents:
+   (let
+       ([fname (bytes->string/utf-8 (binding:file-filename binds))]
+        [fcontents (binding:file-content binds)])
+     (values fname fcontents))))
+
+ ; start: request -> doesn't return
+(define (start request)
+  (render-uploader request))
 
 (serve/servlet start
                #:servlet-path "/"
