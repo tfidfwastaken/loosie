e46328a471c725d06a7e0b2ccb81514678973ec3
Atharva Raykar <raykar.ath@gmail.com>
2020-07-11 00:08:52 +0530

    Add tests for model
    Finally forced myself to write tests (which I thought seemed too
    extra)
    
    Obviously it was not extra because I found bugs in the regexp replace
    this way .____.

--- model.rkt
+++ model.rkt
@@ -8,6 +8,9 @@
          crypto/libcrypto
          racket/string)
 
+(module+ test
+  (require rackunit))
+
 (provide init-db port app-url
          (schema-out loosie)
          upload-file get-mime-type
@@ -61,11 +64,9 @@
 
 ; creates url-safe access code
 (define (make-access-code)
-  (let* ([b64-code (base64-encode (crypto-random-bytes 9) #"")]
-         [no-plus (regexp-replace #rx"\\+" b64-code "-")]
-         [no-slash (regexp-replace #rx"/" no-plus "_")]
-         [no-equal (regexp-replace #rx"=" no-slash "*")])
-    (bytes->string/utf-8 no-equal)))
+  (regexp-replaces (bytes->string/utf-8
+                    (base64-encode (crypto-random-bytes 9) #""))
+                   '([#rx"\\+" "-"] [#rx"/" "_"] [#rx"=" "*"])))
 
 (define (get-password-hash pw)
   (pwhash '(pbkdf2 hmac sha256)
@@ -79,3 +80,47 @@
 
 (define (valid-password? pw)
   (regexp-match? #px"^[ 0-9A-Za-z!@#$%^&*()]{8,32}$" pw))
+
+(module+ test
+  (test-case
+      "checks if mime-types match correctly"
+    (check-equal? 'text/html
+                  (get-mime-type "abcd.html"))
+    (check-equal? 'text/html
+                  (get-mime-type "ble/h.html"))
+    (check-equal? 'text/html
+                  (get-mime-type "bleh.htm"))
+    (check-equal? 'text/markdown
+                  (get-mime-type "ghanshyam.md"))
+    (check-equal? 'text/plain
+                  (get-mime-type "baburao.txt"))
+    (check-equal? 'application/pdf
+                  (get-mime-type "aman_rashid.pdf"))
+    (check-equal? 'unknown
+                  (get-mime-type "freedom is an illusion")))
+
+  (test-case
+      "checks if access codes are cleaned properly"
+    (define-simple-check (check-not-regexp-match pxstr str)
+      (not (regexp-match? (pregexp pxstr) str)))
+    (check-not-regexp-match "[+=/]"
+                            (for/fold ([combined ""])
+                                      ([i (in-range 30)])
+                              (string-append combined (make-access-code)))))
+
+  (test-case
+      "check password validity"
+    (check-true (valid-password? "fast and bulbous"))
+    (check-true (valid-password? "#I u5e (em@*$^)!"))
+    (check-false (valid-password? "bulbous-also-tapered"))
+    (check-false (valid-password? "2short"))
+    (check-false (valid-password? "this passphrase is too heccin long"))
+    (check-false (valid-password? "nuh/uh"))
+    (check-false (valid-password? "passphrasen't"))
+    (check-false (valid-password? ":[")))
+
+  (test-case
+      "check password verification"
+    (check-true
+     (verify-password "a password"
+                      (get-password-hash "a password")))))
